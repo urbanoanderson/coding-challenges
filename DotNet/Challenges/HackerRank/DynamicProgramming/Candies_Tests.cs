596c96e3f5ecb6e4aa35af146a805207c86d3d28
Anderson Urbano <anderson.urb01@gmail.com>
2019-06-06 18:19:35 -0300

    Solve Candies problem but big test needs performance optimization

--- DotNet/Challenges/HackerRank/DynamicProgramming/Candies_Tests.cs
+++ DotNet/Challenges/HackerRank/DynamicProgramming/Candies_Tests.cs
@@ -12,10 +12,24 @@ namespace HackerRank.Candies
         [TestCase(new int[] { 4, 6, 4, 5, 6, 2 }, 10)]
         [TestCase(new int[] { 2, 4, 3, 5, 2, 6, 4, 5 }, 12)]
         [TestCase(new int[] { 2, 4, 2, 6, 1, 7, 8, 9, 2, 1 }, 19)]
-        public void Tests(int[] input, int expected)
+        public void Tests(int[] input, long expected)
         {
-            Candies candies = new Candies();
-            long result = candies.CalculateMinCandies(input);
+            long result = (new Candies()).CalculateMinCandies(input);
+            Assert.IsTrue(result == expected);
+        }
+
+        [Test]
+        public void TestBig()
+        {
+            int arraySize = 100000;
+            int[] input = new int[arraySize];
+            long expected = 5000050000;
+
+            for (int i = 0; i < arraySize; i++)
+                input[i] = arraySize - i;
+
+            long result = (new Candies()).CalculateMinCandies(input);
+
             Assert.IsTrue(result == expected);
         }
     }
