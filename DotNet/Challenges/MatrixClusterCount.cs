251c19f740bb8c6101773267126153053602caea
Anderson Urbano <anderson.urb01@gmail.com>
2019-11-20 12:50:13 -0300

    complete matrix cluster count challenge

--- DotNet/Challenges/MatrixClusterCount.cs
+++ DotNet/Challenges/MatrixClusterCount.cs
@@ -11,14 +11,79 @@
 */
 
 using System;
+using System.Collections.Generic;
 
 namespace Challenges.MatrixClusterCount
 {
+    public class Cluster
+    {
+        IDictionary<Tuple<int, int>, bool> cellMap;
+
+        public Cluster()
+        {
+            this.cellMap = new Dictionary<Tuple<int, int>, bool>();
+        }
+
+        public bool HasKey(int i, int j)
+        {
+            return this.cellMap.ContainsKey(new Tuple<int, int>(i, j));
+        }
+
+        public void AddKey(int i, int j)
+        {
+            this.cellMap.Add(new Tuple<int, int>(i, j), true);
+        }
+    }
+
     public static class MatrixClusterCount
     {
         public static int CountClusters(bool[,] matrix)
         {
-            throw new NotImplementedException();
+            IList<Cluster> clusters = new List<Cluster>();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i,j] == true && !ClustersHaveKey(clusters, i, j))
+                    {
+                        Cluster c = new Cluster();
+                        clusters.Add(c);
+                        ExploreCluster(c, matrix, i, j);
+                    }
+                }
+            }
+
+            return clusters.Count;
+        }
+
+        private static bool ClustersHaveKey(IList<Cluster> clusters, int i, int j)
+        {
+            foreach (Cluster c in clusters)
+            {
+                if (c.HasKey(i, j))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static void ExploreCluster(Cluster c, bool[,] matrix, int i, int j)
+        {
+            if (i < 0 || i >= matrix.GetLength(0))
+                return;
+
+            if (j < 0 || j >= matrix.GetLength(1))
+                return;
+
+            if (matrix[i, j] == true && !c.HasKey(i, j))
+            {
+                c.AddKey(i, j);
+                ExploreCluster(c, matrix, i-1, j);
+                ExploreCluster(c, matrix, i+1, j);
+                ExploreCluster(c, matrix, i, j-1);
+                ExploreCluster(c, matrix, i, j+1);
+            }
         }
     }
 }
--- DotNet/Tests/MatrixClusterCount_Tests.cs
+++ DotNet/Tests/MatrixClusterCount_Tests.cs
@@ -6,14 +6,47 @@ namespace Challenges.MatrixClusterCount
     {
         private  static readonly object[] testcases =
         {
-            new object[] { new bool[,] { { true, true }, { false, false } }, 1 },
+            new object[] { new bool[,] { { false } }, 0 },
             new object[] { new bool[,] { { true }, { false } }, 1 },
+            new object[] { new bool[,] { { true, true }, { false, false } }, 1 },
+            new object[] { new bool[,] { { true, false, true }, { false, false, false }, { true, false, true } }, 4 },
+            new object[] { new bool[,] {
+                { true, true, true },
+                { false, false, true },
+                { true, false, true }
+               }, 2 },
+            new object[] { new bool[,] {
+                { true, true, false, false },
+                { false, false, true, false },
+                { false, false, false, false },
+                { true, true, true, true },
+                }, 3 },
+            new object[] { new bool[,] {
+                { true, false, true, false },
+                { false, true, false, true },
+                { true, false, true, false },
+                }, 6 },
+            new object[] { new bool[,] {
+                { true, false, false },
+                { false, true, false },
+                { false, false, true }
+               }, 3 },
+            new object[] { new bool[,] {
+                { false, false, true, false },
+                { true, false, true, false },
+                { true, true, false, false },
+                }, 2 },
+            new object[] { new bool[,] {
+                { true, true, true },
+                { true, true, true },
+                { true, true, true }
+               }, 1 },
         };
 
         [Test, TestCaseSource(nameof(testcases))]
-        public void Tests(bool[,] input, long expected)
+        public void Tests(bool[,] input, int expected)
         {
-            long result = MatrixClusterCount.CountClusters(input);
+            int result = MatrixClusterCount.CountClusters(input);
             Assert.IsTrue(result == expected);
         }
     }
