26779e47e99ca1c300c810991bd06514cf1d5f30
Anderson Urbano <anderson.urb01@gmail.com>
2020-09-27 18:31:51 -0300

    implement array manipulation

--- DotNet/Challenges/ArrayManipulation.cs
+++ DotNet/Challenges/ArrayManipulation.cs
@@ -10,6 +10,7 @@ namespace Challenges.ArrayManipulation
 {
     public class ArrayManipulation
     {
+        /* Calculates solution by adding the slope increases instead of keeping track of all values*/
         public static long Solution(int n, int[][] queries)
         {
             long[] array = new long[n];
@@ -18,19 +19,23 @@ namespace Challenges.ArrayManipulation
                 array[i] = 0;
             }
 
+            // each query consists of [a, b, k]
             foreach (var query in queries)
             {
-                for (int j = query[0]-1; j < query[1]; j++)
-                {
-                    array[j] += query[2];
-                }
+                // array[a] += k
+                array[query[0]-1] += query[2];
+
+                // array[b] -= k (if b is not the last element)
+                if (query[1] < array.Length)
+                    array[query[1]] -= query[2];
             }
 
-            long maxValue = array[0];
+            long sum = 0;
+            long maxValue = long.MinValue;
             for (int i = 0; i < n; i++)
             {
-                if (array[i] > maxValue)
-                    maxValue = array[i];
+                sum += array[i];
+                maxValue = Math.Max(maxValue, sum);
             }
 
             return maxValue;
