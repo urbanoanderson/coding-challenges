c6bc00aa6b7835110023c3cc68057371b80e68ee
Anderson Urbano <anderson.urb01@gmail.com>
2021-04-17 19:27:26 -0300

    create test cases

---
+++ Solutions/FormingMagicSquare.cs
@@ -0,0 +1,20 @@
+﻿/*
+    Source: https://www.hackerrank.com/challenges/magic-square-forming
+
+    Solution Author: Anderson Urbano
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Challenges.Solutions.FormingMagicSquare
+{
+    public static class FormingMagicSquare
+    {
+        public static int Solution(int[][] matrix)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
