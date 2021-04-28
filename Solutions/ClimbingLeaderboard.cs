10b5986713abbc081e5e3a076a44e054321188d5
Anderson Urbano <anderson.urb01@gmail.com>
2021-04-27 21:20:04 -0300

    create unit tests

---
+++ Solutions/ClimbingLeaderboard.cs
@@ -0,0 +1,19 @@
+﻿/*
+    Source: https://www.hackerrank.com/challenges/climbing-the-leaderboard
+
+    Solution Author: Anderson Urbano
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Challenges.Solutions.ClimbingLeaderboard
+{
+    public static class ChallengeSolution
+    {
+        public static List<int> Solve(List<int> ranked, List<int> player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
